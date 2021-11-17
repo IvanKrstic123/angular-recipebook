602ba06aed41fb00c45395f140d1dd577059c1f8
Ivan Krstic <krstic8687@yahoo.com>
2021-11-17 19:05:26 +0100

    Project Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,25 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { DirectivesAssigmentComponent } from './directives-assigment/directives-assigment.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShopingListComponent } from './shoping-list/shoping-list.component';
+import { ShopingEditComponent } from './shoping-list/shoping-edit/shoping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    DirectivesAssigmentComponent,
-  ],
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+  RecipeDetailComponent,
+    RecipeItemComponent,
+    ShopingListComponent,
+    ShopingEditComponent
+    ],
   imports: [
     BrowserModule,
     FormsModule
--- src/app/shoping-list/shoping-list.component.spec.ts
+++ src/app/shoping-list/shoping-list.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ServersComponent } from './servers.component';
+import { ShopingListComponent } from './shoping-list.component';
 
-describe('ServersComponent', () => {
-  let component: ServersComponent;
-  let fixture: ComponentFixture<ServersComponent>;
+describe('ShopingListComponent', () => {
+  let component: ShopingListComponent;
+  let fixture: ComponentFixture<ShopingListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ServersComponent ]
+      declarations: [ ShopingListComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ServersComponent);
+    fixture = TestBed.createComponent(ShopingListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
